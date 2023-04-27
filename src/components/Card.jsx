f8b67ebedf816061f11aceef1d44c2a4c99bff93
damiro <hello.damiro@gmail.com>
2023-04-27 22:31:55 +0800

    game section grid

---
+++ src/components/Card.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Card() {
+    return (
+        <div className="card">
+            <img src="../assets/images/runes/rune-defense.svg" alt="rune"></img>
+            <h3>Defense</h3>
+        </div>
+    );
+}
+
+export default Card;
--- src/components/Game.jsx
+++ src/components/Game.jsx
@@ -1,7 +1,47 @@
 import React from 'react';
+import Card from './Card';
 
 function Game() {
-    return <main>game</main>;
+    return (
+        <main>
+            <div className="game">
+                <div className="cards">
+                    <Card />
+                    <Card />
+                    <Card />
+                    <Card />
+                    <Card />
+                    <Card />
+                    <Card />
+                    <Card />
+                    <Card />
+                    <Card />
+                    <Card />
+                    <Card />
+                    <Card />
+                    <Card />
+                    <Card />
+                    <Card />
+                    <Card />
+                    <Card />
+                    <Card />
+                    <Card />
+                    <Card />
+                    <Card />
+                    <Card />
+                    <Card />
+                </div>
+            </div>
+            <div className="score-board">
+                <p className="score">2</p>
+                <p>
+                    Symbolizes strength, vitality, and courage, signifying inner power and
+                    determination.
+                </p>
+                <p className="high-score">100</p>
+            </div>
+        </main>
+    );
 }
 
 export default Game;
